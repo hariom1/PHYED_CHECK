425ee47ac63b2cb141e7aeb77252e9e22c6088b5
HARIOM <hariomk628@gmail.com>
2024-05-07 02:46:29 +0530

    chnages

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ echo "Making migrations..."
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
-echo "Collect static files..."
+echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput --clear
 
 
