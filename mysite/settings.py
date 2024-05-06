2bff5a12b9860fccea3d742cc568527225aca976
HARIOM <hariomk628@gmail.com>
2024-05-07 03:12:41 +0530

    hari

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,7 +124,9 @@ DATABASES = {
     }
 }
  
-DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config(default='postgres://...',
+    conn_max_age=600,
+    conn_health_checks=True,) 
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
